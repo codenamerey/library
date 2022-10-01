cb967eb7a2fc308378a9efb760493a7821a7657d
Lee Ryan <2solimanleeryan@gmail.com>
2022-10-02 00:05:45 +0800

    Toggle read status on checkbox click

--- js/script.js
+++ js/script.js
@@ -18,27 +18,62 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
+Book.prototype.toggleReadStatus = function() {
+    this.readStatus = !this.readStatus;
+}
+
 function addToLibrary(title, author, pages, readStatus) {
     let newBook = new Book(title, author, pages, readStatus);
     booksList.push(newBook);
 }
 
 function updateLibrary() {
+    let id = 0;
     booksDisplay.innerHTML = '';
     booksList.forEach(book => {
         const row = document.createElement('tr');
         row.className = 'book';
         booksDisplay.appendChild(row);
+        bookLoop:
         Object.values(book).forEach((value) => {
+            let id = 0;
+            if(value == true || value == false) {
+                const button_switch = document.createElement('input');
+                button_switch.type = 'checkbox';
+                button_switch.classList.add('readStatus');
+                if(value == true) {
+                    button_switch.classList.add('checked');
+                }
+                button_switch.id = id;
+                button_switch.addEventListener('click', toggleReadStatus);
+                row.appendChild(button_switch);
+                id++;
+                return;
+            }
             const cell = document.createElement('td');
             cell.textContent = value;
             row.appendChild(cell);
         });
+        const delete_cell = document.createElement('td');
+        const delete_button = document.createElement('button');
+        delete_button.textContent = 'Delete';
+        delete_button.id = id;
+        delete_cell.appendChild(delete_button);
+        row.appendChild(delete_cell);
+        id++;
     });
 }
 
+function toggleReadStatus(e) {
+    e.target.classList.toggle('checked');
+    const book = booksList[e.target.id];
+    book.toggleReadStatus();
+}
+
+addToLibrary('dawd','dawda', 'true', false);
 updateLibrary();
 formTrigger.addEventListener('click', () => {
     bookForm.classList.add('active');
     document.body.classList.add('active');
-})
+});
+
