0609e3b5ae363a3e74def8b93b2c751b974928f7
Lee Ryan <2solimanleeryan@gmail.com>
2022-10-02 00:28:34 +0800

    Delete object from array on "delete" button click

--- js/script.js
+++ js/script.js
@@ -2,14 +2,7 @@ const booksDisplay = document.querySelector('#books');
 const formTrigger = document.querySelector('#form-trigger');
 const bookForm = document.querySelector('#add-book-form');
 
-let booksList = [
-    {
-        title: "Harry Potter",
-        author: "J.K. Rowling",
-        pages: "200",
-        readStatus: true
-    }
-]
+let booksList = [];
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -29,14 +22,13 @@ function addToLibrary(title, author, pages, readStatus) {
 
 function updateLibrary() {
     let id = 0;
+    let button_id = 0;
     booksDisplay.innerHTML = '';
     booksList.forEach(book => {
         const row = document.createElement('tr');
         row.className = 'book';
         booksDisplay.appendChild(row);
-        bookLoop:
         Object.values(book).forEach((value) => {
-            let id = 0;
             if(value == true || value == false) {
                 const button_switch = document.createElement('input');
                 button_switch.type = 'checkbox';
@@ -44,10 +36,10 @@ function updateLibrary() {
                 if(value == true) {
                     button_switch.classList.add('checked');
                 }
-                button_switch.id = id;
+                button_switch.id = button_id;
+                button_id++;
                 button_switch.addEventListener('click', toggleReadStatus);
                 row.appendChild(button_switch);
-                id++;
                 return;
             }
             const cell = document.createElement('td');
@@ -58,6 +50,8 @@ function updateLibrary() {
         const delete_button = document.createElement('button');
         delete_button.textContent = 'Delete';
         delete_button.id = id;
+        delete_button.className = 'delete_buttons';
+        delete_button.addEventListener('click', deleteFromLibrary);
         delete_cell.appendChild(delete_button);
         row.appendChild(delete_cell);
         id++;
@@ -66,10 +60,16 @@ function updateLibrary() {
 
 function toggleReadStatus(e) {
     e.target.classList.toggle('checked');
-    const book = booksList[e.target.id];
+    let book = booksList[e.target.id];
     book.toggleReadStatus();
 }
 
+function deleteFromLibrary(e) {
+    booksList.splice(e.target.id, 1);
+    updateLibrary();
+}
+
+addToLibrary('Harry Potter', 'J.K. Rowling', 200, true);
 addToLibrary('dawd','dawda', 'true', false);
 updateLibrary();
 formTrigger.addEventListener('click', () => {
